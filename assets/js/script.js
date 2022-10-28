7864865137cc07884033cd29423b55a0a700e756
jazze13 <75473825+jazze13@users.noreply.github.com>
2022-10-28 10:28:17 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,45 @@
 "use strict";
 
+
+// выполнение по нажатию на кнопку с задержкой
+const functionButtons = document.querySelectorAll('.functions_list__button');
+functionButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        setTimeout(() => {
+
+            switch(this.id) {
+                case 'exercise':
+                    exercise();
+                    break;
+                case 'if_else':
+                    if_else();
+                    break;
+                case 'and_or':
+                    and_or();
+                    break;
+                case 'switch_case':
+                    switch_case();
+                    break;
+            }
+
+        }, 500)
+    })
+})
+
+
 //  Задачи
 function exercise() {
 
     // 1
     let a = +prompt('min = ', 10);
-    a == 10 ? alert('a == 10 верно') : alert('a == 10 неверно');
+    a == 10 ? console.log('a == 10 верно') : console.log('a == 10 неверно');
 
     // 2
     let min = +prompt('min = ', 20);
-    min >= 0 && min <= 15 ? alert('1 четверть') :
-    min >= 16 && min <= 30 ? alert('2 четверть') :
-    min >= 31 && min <= 45 ? alert('3 четверть') :
-    min >= 46 && min <= 60 ? alert('4 четверть') : () => {};
+    min >= 0 && min <= 15 ? console.log('1 четверть') :
+    min >= 16 && min <= 30 ? console.log('2 четверть') :
+    min >= 31 && min <= 45 ? console.log('3 четверть') :
+    min >= 46 && min <= 60 ? console.log('4 четверть') : () => {};
 
     // 3
     // через if
@@ -45,14 +72,14 @@ function exercise() {
 function if_else() {
     let a = +prompt('a = ', 10);
 
-    a == 0 ? alert('a == 0 верно') : alert('a == 0 неверно');
-    a > 0 ? alert('a > 0 верно') : alert('a > 0 неверно');
-    a < 0 ? alert('a < 0 верно') : alert('a < 0 неверно');
-    a >= 0 ? alert('a >= 0 верно') : alert('a >= 0 неверно');
-    a <= 0 ? alert('a <= 0 верно') : alert('a <= 0 неверно');
-    a != 0 ? alert('a != 0 верно') : alert('a != 0 неверно');
-    a == 'test' ? alert("a == 'test' верно") : alert("a == 'test' неверно");
-    a === '1' ? alert("a === '1' верно") : alert("a === '1' неверно");
+    a == 0 ? console.log('a == 0 верно') : console.log('a == 0 неверно');
+    a > 0 ? console.log('a > 0 верно') : console.log('a > 0 неверно');
+    a < 0 ? console.log('a < 0 верно') : console.log('a < 0 неверно');
+    a >= 0 ? console.log('a >= 0 верно') : console.log('a >= 0 неверно');
+    a <= 0 ? console.log('a <= 0 верно') : console.log('a <= 0 неверно');
+    a != 0 ? console.log('a != 0 верно') : console.log('a != 0 неверно');
+    a == 'test' ? console.log("a == 'test' верно") : console.log("a == 'test' неверно");
+    a === '1' ? console.log("a === '1' верно") : console.log("a === '1' неверно");
 }
 
 // Работа с && (и) и || (или)
@@ -60,27 +87,27 @@ function and_or() {
     let a = +prompt('a = ', 10);
     
     // 1
-    a > 0 && a < 5 ? alert('1: верно') : alert('1: неверно');
+    a > 0 && a < 5 ? console.log('1: верно') : console.log('1: неверно');
 
     // 2
     if (a == 0 || a == 2) {
         a += 7;
-        alert('2: ' + a);
+        console.log('2: ' + a);
     }
 
     // 3
     let b = +prompt('b = ', 1);
     if (a <= 1 && b >= 3) {
-        alert( '3: ' + (a + b) );
+        console.log( '3: ' + (a + b) );
     } else {
-        alert( '3: ' + (a - b) );
+        console.log( '3: ' + (a - b) );
     }
 
     // 4
     if ( (a > 2 && a < 11) || (b >= 6 && b < 14) ) {
-        alert('4: верно');
+        console.log('4: верно');
     } else {
-        alert('4: неверно');
+        console.log('4: неверно');
     }
 }
 
@@ -104,19 +131,19 @@ function switch_case() {
             result = 'осень';
             break;
     }
-    alert('1: result = ' + result);
+    console.log('1: result = ' + result);
 
     // 2
     let day = prompt('day = ', 10);
-    day >= 1 && day <= 10 ? alert('2: 1') :
-    day >= 11 && day <= 20 ? alert('2: 2') : alert('2: 3');
+    day >= 1 && day <= 10 ? console.log('2: 1') :
+    day >= 11 && day <= 20 ? console.log('2: 2') : console.log('2: 3');
 
     // 3
     let month = prompt('month = ', 2);
-    month >= 1 && month <= 2 || month == 12 ? alert('3: зима') :
-    month >= 3 && month <= 5 ? alert('3: весна') :
-    month >= 6 && month <= 8 ? alert('3: лето') :
-    month >= 9 && month <= 11 ? alert('3: осень') : doNothing();
+    month >= 1 && month <= 2 || month == 12 ? console.log('3: зима') :
+    month >= 3 && month <= 5 ? console.log('3: весна') :
+    month >= 6 && month <= 8 ? console.log('3: лето') :
+    month >= 9 && month <= 11 ? console.log('3: осень') : doNothing();
     
     function doNothing() {
         // nothing
@@ -124,7 +151,7 @@ function switch_case() {
 
     // 4 
     let str = 'abcde';
-    str[1] == 'a' ? alert('4: да') : alert('4: нет');
+    str[1] == 'a' ? console.log('4: да') : console.log('4: нет');
 
     // 5
     str = '12345';
@@ -132,23 +159,23 @@ function switch_case() {
         case '1':
         case '2':
         case '3':
-            alert('5: да');
+            console.log('5: да');
             break;
         default:
-            alert('5: нет');
+            console.log('5: нет');
             break;
     }
 
     // 6
     str = '582';
-    alert( '6: ' + (str[0] + str[1] + str[2]) );
+    console.log( '6: ' + (str[0] + str[1] + str[2]) );
 
     // 7
     str = '894326';
     if ( str[0] + str[1] + str[2] == str[3] + str[4] + str[5] )
-        alert('7: да')
+        console.log('7: да')
     else
-        alert('7: нет');
+        console.log('7: нет');
 }
 
 
